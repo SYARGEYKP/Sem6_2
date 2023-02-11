f836e0e7afef12520c6178105cb0eeacffead1ae
SYARGEY <lss1895@mail.ru>
2023-02-11 17:18:52 +0300

    Семинар 6 задание 2

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+int InputInt(string massage)
+{
+    System.Console.WriteLine($"{massage} ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+(double, double) Peresech(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return (x, y);
+}
+bool Chek(double k1, double k2)
+{
+    if (k1 == k2)
+    {
+        System.Console.WriteLine("прямые параллельны или совпадают");
+        return false;
+    }
+    return true;
+}
+int b1 = InputInt("Введите b1: ");
+int k1 = InputInt("Введите k1: ");
+int b2 = InputInt("Введите b2: ");
+int k2 = InputInt("Введите k2: ");
+
+if (Chek(k1, k2))
+{
+    (double x, double y) = Peresech(b1, k1, b2, k2);
+    System.Console.WriteLine($"Пересечение прямых в точке: {x}; {y}");
+}
+
